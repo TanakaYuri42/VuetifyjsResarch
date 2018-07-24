50946a4e788a842f3aad5be6307aab2087000f68
y-tanaka <y-tanaka@letsiwahana.ttcad.local>
2018-07-24 16:10:40 +0900

    iterator

--- src/components/pages/index.vue
+++ src/components/pages/index.vue
@@ -159,24 +159,11 @@
             <v-card>
               <v-card-text>
                 <div>
-
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/en/components/breadcrumbs" target="_blank">v-breadcrumbs</a>
+                  」参照<br>
+                  アイコン表示や、アイコンによる区切りも設定できる
                 </div>
-                <v-data-table
-                  :headers="headers"
-                  :items="breadcrumbs_list"
-                  hide-actions
-                  class="elevation-1" 
-                  hide-headers="true"
-                >
-                  <template slot="items" slot-scope="props">
-                      <td>
-                        <a :href="props.item.url" target="_blank">
-                          {{ props.item.name }}
-                        </a>
-                      </td>
-                    <td>{{ props.item.text }}</td>
-                  </template>
-                </v-data-table>
               </v-card-text>
             </v-card>
           </v-expansion-panel-content>
@@ -478,6 +465,197 @@
             </v-card>
           </v-expansion-panel-content>
 
+          <v-expansion-panel-content>
+            <div slot="header">拡張パネル</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/expansion-panels" target="_blank">
+                    v-expansion-panel
+                  </a>
+                  」参照<br>
+                </div>
+                <v-data-table
+                  :headers="headers"
+                  :items="panel_list"
+                  hide-actions
+                  class="elevation-1" 
+                  hide-headers="true"
+                >
+                  <template slot="items" slot-scope="props">
+                      <td>
+                        <a :href="props.item.url" target="_blank">
+                          {{ props.item.name }}
+                        </a>
+                      </td>
+                    <td>{{ props.item.text }}</td>
+                  </template>
+                </v-data-table>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">アバター</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/avatars" target="_blank">
+                    v-avatar
+                  </a>
+                  」参照<br>
+                  ユーザーの操作に合わせて、動的に要素のサイズ変更や角丸処理を行う
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">テーブル</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/data-tables" target="_blank">
+                    v-data-table
+                  </a>
+                  」参照<br>
+                  表形式でデータ表示。ソート、検索、ページネーション、編集、行選択などの機能を含んでいる
+                </div>
+                <v-data-table
+                  :headers="headers"
+                  :items="table_list"
+                  hide-actions
+                  class="elevation-1" 
+                  hide-headers="true"
+                >
+                  <template slot="items" slot-scope="props">
+                      <td>
+                        <a :href="props.item.url" target="_blank">
+                          {{ props.item.name }}
+                        </a>
+                      </td>
+                    <td>{{ props.item.text }}</td>
+                  </template>
+                </v-data-table>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">スナックバー</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/snackbars" target="_blank">
+                    v-snackbar
+                  </a>
+                  」参照<br>
+                  ユーザーに対して、短いメッセージを表示する。実装に合わせて色の変更も可能
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        
+          <v-expansion-panel-content>
+            <div slot="header">パララックス</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/parallax" target="_blank">
+                    v-parallax
+                  </a>
+                  」参照<br>
+                  画像をウィンドウよりもスクロールして見えるようにする3Dエフェクトを作成する
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">ボトムシート</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/bottom-sheets" target="_blank">
+                    v-bottom-sheet
+                  </a>
+                  」参照<br>
+                  スクリーンの下部からスライドするように表示されるシート
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">カルーセル</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/carousels" target="_blank">
+                    v-carousel
+                  </a>
+                  」参照<br>
+                  時間経過や操作によって画像が入れ替わる、大画面のビジュアルコンテンツ
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">ステッパー</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/steppers" target="_blank">
+                    v-stepper
+                  </a>
+                  」参照<br>
+                  ユーザーが番号付きのステップを経て、操作を進行するための画面
+                </div>
+                <v-data-table
+                  :headers="headers"
+                  :items="stepper_list"
+                  hide-actions
+                  class="elevation-1" 
+                  hide-headers="true"
+                >
+                  <template slot="items" slot-scope="props">
+                      <td>
+                        <a :href="props.item.url" target="_blank">
+                          {{ props.item.name }}
+                        </a>
+                      </td>
+                    <td>{{ props.item.text }}</td>
+                  </template>
+                </v-data-table>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
+          <v-expansion-panel-content>
+            <div slot="header">イテレータ</div>
+            <v-card>
+              <v-card-text>
+                <div>
+                  基本的な概要は「
+                  <a href="https://vuetifyjs.com/ja/components/data-iterator" target="_blank">
+                    v-data-iterator
+                  </a>
+                  」参照<br>
+                  データ表示を細かくカスタマイズする。テーブルと似ている
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+
         </v-expansion-panel>
 
       </div>
@@ -565,6 +743,12 @@
             name: 'ツールバー',
             text: "ツールバーとボタンを融合させる",
             url: "https://vuetifyjs.com/ja/components/buttons#example-app-bar"
+          },
+          {
+            value: false,
+            name: 'フローティングアクションボタン',
+            text: "ユーザーにアクションを促すためのボタン",
+            url: "https://vuetifyjs.com/ja/components/floating-action-buttons"
           },
         ],
         icon_list: [
@@ -848,6 +1032,12 @@
             text: "拡張機能を含んだリスト",
             url: "https://vuetifyjs.com/ja/components/lists#example-expansion-lists"
           },  
+          {
+            value: false,
+            name: 'サブヘッダー',
+            text: "リストのセクションを区切るために使用される、サブヘッダーの編集をする",
+            url: "https://vuetifyjs.com/ja/components/subheaders"
+          },  
         ],
         pagination_list:[
           {
@@ -1081,6 +1271,192 @@
             name: 'システムバー',
             text: "システムバーを作成する",
             url: "https://vuetifyjs.com/ja/components/toolbars#example-system-bars-status-window"
+          },  
+        ],
+        panel_list: [
+          {
+            value: false,
+            name: '展開パネル',
+            text: "閉じるまでは開いたままのパネル",
+            url: "https://vuetifyjs.com/ja/components/expansion-panels#example-expand"
+          }, 
+          {
+            value: false,
+            name: '外部コントロール',
+            text: "ボタンなど、パネル外からの操作でパネルを開閉する",
+            url: "https://vuetifyjs.com/ja/components/expansion-panels#example-external"
+          }, 
+          {
+            value: false,
+            name: '無効、読み取り専用',
+            text: "開閉無効のパネル及び、読み取り専用のパネル",
+            url: "https://vuetifyjs.com/ja/components/expansion-panels#example-disabled-and-readonly"
+          }, 
+          {
+            value: false,
+            name: 'ポップアウトとインセット',
+            text: "パネルが外に飛び出すように開いたり、中に引っ込むように開いたりする",
+            url: "https://vuetifyjs.com/ja/components/expansion-panels#example-popout"
+          }, 
+          {
+            value: false,
+            name: 'フォーカス',
+            text: "パネルを開いた際に、目立つようフォーカスする",
+            url: "https://vuetifyjs.com/ja/components/expansion-panels#example-focusable"
+          }, 
+        ],
+        table_list: [
+          {
+            value: false,
+            name: 'データがない時の設定',
+            text: "表のデータがない時に表示するhtmlを設定する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-no-data"
+          }, 
+          {
+            value: false,
+            name: 'ヘッダーのセルごとの共通設定',
+            text: "ヘッダーのセルごとに共通のマークアップやエフェクトを設定する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-header-cell"
+          }, 
+          {
+            value: false,
+            name: 'プログレス',
+            text: "データをロードしている時にプレグレスを表示する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-progress"
+          }, 
+          {
+            value: false,
+            name: 'フッター',
+            text: "テーブルのフッター。カラムのフィルタや検索など、テーブルに拡張機能を持たせたい時に使う",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-footer"
+          },
+          {
+            value: false,
+            name: '行の折りたたみ',
+            text: "行を選択した際に、行の開閉をできるようにする",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-expand"
+          },  
+          {
+            value: false,
+            name: 'ページネーションの編集',
+            text: "ページネーションで使用されるテキストを変更する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-page-text"
+          },  
+          {
+            value: false,
+            name: '行の選択',
+            text: "行を選択可能にして、行に対してアクションを実行できる",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-select"
+          },  
+          {
+            value: false,
+            name: '行の検索',
+            text: "検索機能をつける",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-search"
+          },  
+          {
+            value: false,
+            name: 'アイコン',
+            text: "ページネーションで、ページ移動のボタンをアイコンにする",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-custom-icons"
+          },  
+          {
+            value: false,
+            name: '外部ページネーション',
+            text: "テーブル外部からページネーションを制御する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-paginate"
+          },  
+          {
+            value: false,
+            name: '外部ソート',
+            text: "テーブル外部からソートを制御する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-sort"
+          },  
+          {
+            value: false,
+            name: 'サーバサイドからのページネーション・ソート',
+            text: "バックエンドから情報を読み込み、結果を表示する前にページネーションやソートを適用する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-server"
+          },  
+          {
+            value: false,
+            name: 'ヘッダーの非表示',
+            text: "ヘッダーなしのテーブルを作成する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-headerless"
+          },  
+          {
+            value: false,
+            name: 'インライン編集',
+            text: "テーブル内部でインライン編集する",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-editdialog"
+          },  
+          {
+            value: false,
+            name: '各行の編集',
+            text: "行の新規追加・編集・削除をする",
+            url: "https://vuetifyjs.com/ja/components/data-tables#example-crud"
+          },  
+        ],
+        stepper_list: [
+          {
+            value: false,
+            name: '編集可能なステップ',
+            text: "編集可能なステップなら、ユーザーの選択で移動できる",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-editable"
+          },  
+          {
+            value: false,
+            name: '編集不可能なステップ',
+            text: "ユーザーの選択で移動することはできず、ステップは直線的に進行する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-non-editable"
+          },  
+          {
+            value: false,
+            name: 'オプションステップ',
+            text: "サブテキストを読み込む",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-optional"
+          },  
+          {
+            value: false,
+            name: '水平ステップ',
+            text: "定義されたステップを経て、x軸に沿って進行する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-horizontal"
+          }, 
+          {
+            value: false,
+            name: '垂直ステップ',
+            text: "定義されたステップを経て、y軸に沿って進行する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-vertical"
+          },  
+          {
+            value: false,
+            name: 'リニアステップ',
+            text: "常に定義されたステップで進行する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-linear"
+          },  
+          {
+            value: false,
+            name: '非リニアステップ',
+            text: "ユーザーが選択した順番でステップが進行する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-non-linear"
+          },  
+          {
+            value: false,
+            name: 'エラー表示',
+            text: "エラーが起きた際に、それを表示する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-error"
+          },  
+          {
+            value: false,
+            name: '代替ラベルによるエラー表示',
+            text: "エラー状態を代替ラベルスタイルに適用する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-alternate-error"
+          },  
+          {
+            value: false,
+            name: 'ダイナミックなステップ',
+            text: "ユーザーの操作で、動的にステップを追加・削除する",
+            url: "https://vuetifyjs.com/ja/components/steppers#example-dynamic"
           },  
         ]
       }
